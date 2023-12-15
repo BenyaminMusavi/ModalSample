3b7a1f5ccb919fd26a7efcc1a771e174cbf15571
BenyaminMusavi <beni97d@gmail.com>
2023-12-15 14:41:57 +0330

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,126 @@
+﻿using ExcelDataReader;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(IFormFile file)
+    {
+        if (ModelState.IsValid)
+        {
+            if (file != null && file.Length > 0)
+            {
+                if (file.FileName.EndsWith(".xls") || file.FileName.EndsWith(".xlsx"))
+                {
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                    Stream stream = file.OpenReadStream();
+                    IExcelDataReader reader;
+                    if (file.FileName.EndsWith(".xls"))
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    }
+                    else
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+
+                    DataTable dt = CreateDataTable(reader);
+
+                    //await _ImportRepository.SaveToDatabase(dt);
+
+                    //return PartialView("_DataTablePartialView", dt);
+                    //TempData["Dt"] = dt;
+
+                    return View(dt);
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "This file format is not supported");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("File", "Please upload your file");
+            }
+
+        }
+        return View();
+
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    private DataTable CreateDataTable(IExcelDataReader reader)
+    {
+        DataTable dt = new();
+        DataRow row;
+        DataTable dt_ = new();
+        var Date = GetPersianDate();
+
+        try
+        {
+            dt_ = reader.AsDataSet().Tables[0];
+            dt.Columns.Add("PerDate");
+            for (int i = 0; i < dt_.Columns.Count; i++)
+            {
+                dt.Columns.Add(dt_.Rows[0][i].ToString());
+            }
+            for (int row_ = 1; row_ < dt_.Rows.Count; row_++)
+            {
+                row = dt.NewRow();
+                row["PerDate"] = Date;
+
+                for (int col = 0; col < dt_.Columns.Count; col++)
+                {
+                    row[col + 1] = dt_.Rows[row_][col].ToString();
+                }
+                dt.Rows.Add(row);
+            }
+        }
+        catch (Exception)
+        {
+            ModelState.AddModelError("File", "Unable to upload file!");
+        }
+        reader.Close();
+        reader.Dispose();
+        return dt;
+    }
+
+
+
+    public static string GetPersianDate()
+    {
+        System.Globalization.PersianCalendar persianCalandar = new();
+        var dateTime = DateTime.Now;
+        string year = persianCalandar.GetYear(dateTime).ToString().Substring(2, 2);
+        string month = persianCalandar.GetMonth(dateTime).ToString("0#");
+        //int day = persianCalandar.GetDayOfMonth(dateTime);
+        return $"{year}{month}";
+    }
+}
---
+++ WebApplication1/Models/DataModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace WebApplication1.Models
+{
+    public class DataModel
+    {
+        public DataTable  DataTable { get; set; } = new DataTable();
+    }
+}
